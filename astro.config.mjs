df49f93a6224852e11c9d77e7818ff2398109bbc
Luc <luc.longin@gmail.com>
2023-10-11 17:20:10 -0500

    adding i18next library and adding translation of homepage into spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,44 @@
-import { defineConfig } from "astro/config";
+/*import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+});
+*/
+import astroI18next from "astro-i18next";
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-ES",
+  fr: "fr-CA",
+};
+
+export default defineConfig({
+  /*site: "http://localhost:4321/",
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },*/
+  integrations: [
+	react(),
+	astroI18next()
+    /*i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),*/
+  ],
 });
---
+++ src/middleware.js
@@ -0,0 +1,5 @@
+/*import { sequence } from "astro/middleware";
+import { i18nMiddleware } from "astro-i18n-aut";
+
+export const onRequest = sequence(i18nMiddleware);
+*/
